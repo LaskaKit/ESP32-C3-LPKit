d4fea1427dd6235eaba8e6db20925f24ecef9b15
petus <petus@users.noreply.github.com>
2024-08-11 23:43:32 +0200

    Update deepsleep_test.ino
    Serial bug fix

--- SW/deepsleep_test/deepsleep_test.ino
+++ SW/deepsleep_test/deepsleep_test.ino
@@ -4,12 +4,14 @@
 *
 * !!! ESP library version !!!
 * ESP32 library 3.0.x
-* Condition: Tools -> USB CDC On Boot must be enabled
-* use HWCDCSerial instead of USBSerial
+* Condition: Tools -> if USB CDC On Boot is ENABLED then
+* Serial means native USB
+* Tools -> if USB CDC On Boot is DISABLED then
+* Serial means UART
 * ---
 * ESP32 library 2.0.xy
 * Condition: Tools -> USB CDC On Boot must be disabled
-* use USBSerial instead of HWCDCSerial
+* use USBSerial to send data through native USB 
 *
 * Board: ESP32-C3 Dev Module
 */
@@ -19,7 +21,7 @@
 
 void setup(void)
 {
-  USBSerial.begin(115200);
+  Serial.begin(115200);
   pinMode(PIN_ON, OUTPUT);      // Set EN pin for second stabilisator as output
   digitalWrite(PIN_ON, HIGH);   // Turn on the second stabilisator
 
@@ -33,13 +35,13 @@ void loop(void)
 
 void goToSleep(){
 
-  USBSerial.println("I'm not sleeping");
+  Serial.println("I'm not sleeping");
   delay(1000);
   digitalWrite(PIN_ON, LOW);   // Turn on the second stabilisator
-  USBSerial.println("going to sleep 15 sek");
+  Serial.println("going to sleep 15 sek");
   // ESP Deep Sleep 
-  USBSerial.println("ESP in sleep mode");
-  USBSerial.flush(); 
+  Serial.println("ESP in sleep mode");
+  Serial.flush(); 
   esp_sleep_enable_timer_wakeup(SLEEP_SEC * 1000000);
   esp_deep_sleep_start();
 }
