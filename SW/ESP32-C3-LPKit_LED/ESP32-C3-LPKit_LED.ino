c9794d7293d10bdfe293cb01b1d935833c5d9484
Konstantin Láska <konstantin.laska@icloud.com>
2025-06-19 08:50:06 +0200

    Create ESP32-C3-LPKit_LED.ino

---
+++ SW/ESP32-C3-LPKit_LED/ESP32-C3-LPKit_LED.ino
@@ -0,0 +1,46 @@
+/*
+* This example code is used for LaskaKit LaskaKit ESP32-C3-LPKit board  https://www.laskakit.cz/laskkit-esp-12-board/
+* ESP32-C3-LPKit just changing LED color 
+* 
+* Made by (c) laskakit.cz 2025
+* 
+*/
+
+#include <Adafruit_NeoPixel.h>
+#define PIN_ON 4
+#define PIN_LED 9
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PIN_LED, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_ON, OUTPUT);      // Set EN pin for second stabilisator as output
+  digitalWrite(PIN_ON, HIGH);   // Turn on the second stabilisator
+
+  pixels.begin();
+  pixels.setBrightness(10);
+}
+
+void loop() {
+     
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // R, G, B
+  pixels.show();
+
+  delay(1000);
+
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // R, G, B
+  pixels.show();
+
+  delay(1000);
+
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // R, G, B
+  pixels.show();
+
+  delay(1000);
+
+  pixels.setPixelColor(0, pixels.Color(255, 255, 255)); // R, G, B
+  pixels.show();
+
+  delay(1000);
+
+}
